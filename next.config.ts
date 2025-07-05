9ff9f2600710fdf740cf9b1eb2f6424b2bf2d9cc
Josh Thompson <josh.thompsonau@icloud.com>
2025-07-05 11:49:18 +1000

    Prepare for github pages

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,7 @@ const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
   // Uncomment and update if your repo is not at the root (e.g., https://username.github.io/repo-name)
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  // basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   eslint: {
     dirs: ['.'],
   },
